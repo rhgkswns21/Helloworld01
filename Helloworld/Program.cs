f0ed480e60fcce7c7f742647a159d127b38ec3c4
ko <ko@MTES01-42PC>
2019-02-06 10:08:01 +0900

    example.01

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -6,7 +6,10 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
             Console.ReadKey();
         }
     }
